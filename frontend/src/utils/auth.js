8dca5e42133116d99e4b8be03fec036ec592fddd
Kizilov Andrey <irosmith@yandex.ru>
2023-07-12 16:31:09 +0300

    Deploy

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://api.zeromesto.nomoredomains.work";
 
 function request(url, options) {
   const fetchAddress = `${BASE_URL}/${url}`;
